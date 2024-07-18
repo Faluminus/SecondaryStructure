ce703d9240308b7fcf400babaf208c9dc12573b9
Faluminus <semradfilip@email.cz>
2024-07-18 02:45:02 -0700

    threading

--- scripts/buildDataset.py
+++ scripts/buildDataset.py
@@ -1,11 +1,11 @@
 import os
 import logging
 import subprocess
-
+import concurrent.futures
 
 
 #PATHS
-inputFileWIN = 'data/processed/mmCIF'
+inputFileWIN = 'data/raw/mmCIF'
 outputFileWIN = 'data/raw/AMINtoSEC.csv'
 log_dir = 'scripts/logs/'
 
@@ -68,38 +68,55 @@ def ProcessDSSP(dsspData):
     
     return allData
 
+def WorkersRUN(number_of_workers,exec_function):
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=number_of_workers)
+    download_split = DownloadSplit(number_of_workers)
+    for i in range(0,number_of_workers-1):
+        pool.submit(exec_function(download_split[i]))
 
-
+def DownloadSplit(number_of_workers):
+    divided_names = os.listdir(inputFileWIN)
+    x=0
+    for divided in divided_names:
+        proteins = ProteinPaths(inputFileWIN + divided)
+        for protein in proteins:
+            x+=1
+    leftovers = x % number_of_workers
+    split = (x - leftovers) / number_of_workers
+    arr = []
+    for worker in range(0,number_of_workers):
+        if(worker != number_of_workers):
+            range_ = [worker*split - split ,worker*split + leftovers]
+            arr.append(range_)
+        else:
+            range_ = [worker*split - split ,worker*split]
+            arr.append(range_)
+    return arr
 
 
 #MAIN
-divided_names = os.listdir(inputFileWIN)
+def main(to_download):
+    divided_names = os.listdir(inputFileWIN)
+    
+    currently_downloading = 0
+
+
+    for divided in divided_names:
+        proteins = ProteinPaths(inputFileWIN + divided)
+        for protein in proteins:
+            if to_download[0] >= currently_downloading and to_download[1] <= currently_downloading:
+                logger.info(protein + ':processing')
+                args = subprocess.run(CommandDSSP(inputFileWIN + '/' + divided + '/' + protein),capture_output=True, text=True, shell=True).stdout
+                processed_dssp = ProcessDSSP(args)
+                if processed_dssp != ['','']:
+                    finalDataset = open(outputFileWIN,'a',newline="")
+                    finalDataset.write('\n'+processed_dssp[0]+','+processed_dssp[1])
+                    finalDataset.close()
+            currently_downloading +=1
+            
 
 finalDataset = open(outputFileWIN,'a',newline="")
 finalDataset.write("AminoAcidSeq,SecondaryStructureSeq")
 finalDataset.close()
 
-num_of_already_downloaded = CheckForDownloaded()
-currently_downloading = 0
-
-
-for divided in divided_names:
-    proteins = ProteinPaths(inputFileWIN + divided)
-    for protein in proteins:
-        if 23471 < currently_downloading:
-            print(protein + ':doing_DSSP')
-            args = subprocess.run(CommandDSSP(inputFileWIN + '/' + divided + '/' + protein),capture_output=True, text=True, shell=True).stdout
-            print(protein + ':done_DSSP')
-            print(protein + ':procesing_DSSP')
-            processed_dssp = ProcessDSSP(args)
-            print(protein + ':procesing_done_DSSP')
-            if processed_dssp != ['','']:
-                finalDataset = open(outputFileWIN,'a',newline="")
-                finalDataset.write('\n'+processed_dssp[0]+','+processed_dssp[1])
-                finalDataset.close()
-                logger.info(protein + ':was_processed')
-                print(protein + ':was_processed')
-            else:
-                logger.info(protein + ':wasnt_processed')
-                print(protein + ':wasnt_processed')
-        currently_downloading+=1
+WorkersRUN(4,main)
